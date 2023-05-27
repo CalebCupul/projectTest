931b724190816fc73dfea79c82e2ebadca07f524
Caleb Cupul <caleb.cupul@alumnos.udg.mx>
2023-05-26 18:44:58 -0600

    Save Point

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace projectTest.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+
+        private static List<Character> characters = new List<Character>{
+            new Character(),
+            new Character { Name = "Sam", Id = 1}
+        };
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get(){
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id){
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+global using projectTest.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
