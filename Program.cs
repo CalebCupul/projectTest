345a836da5ca480d81bd4fe12f242d89f355c041
Caleb Cupul <caleb.cupul@alumnos.udg.mx>
2023-06-02 18:57:35 -0600

    feat: add db connection

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 global using projectTest.Models;
 global using projectTest.Services.CharacterService;
 global using projectTest.Dtos.Character;
+global using Microsoft.EntityFrameworkCore;
+using projectTest.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
